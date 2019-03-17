199a61fbda2d9dc7c7d37af156f716820cc5df87
Oscar Pérez <mr.omiguelperez@gmail.com>
2019-03-17 16:13:28 -0500

    Circles models, migrations and admin

---
+++ cride/circles/admin.py
@@ -0,0 +1,25 @@
+"""Circles admin."""
+
+from django.contrib import admin
+
+from cride.circles.models import Circle
+
+
+@admin.register(Circle)
+class CircleAdmin(admin.ModelAdmin):
+     """Circle admin."""
+
+     list_display = (
+        'slug_name',
+        'name',
+        'is_public',
+        'verified',
+        'is_limited',
+        'members_limit',
+     )
+     search_fields = ('slug_name', 'name',)
+     list_filter = (
+        'is_public',
+        'verified',
+        'is_limited',
+     )
