8943626586e438adcc255de189f279f1d2237718
Oscar Pérez <mr.omiguelperez@gmail.com>
2019-03-23 19:58:30 -0500

    List and create circle with DRF

--- cride/circles/urls.py
+++ cride/circles/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from cride.circles.views import list_circles
+
+from cride.circles.views import list_circles, create_circle
 
 urlpatterns = [
 	path('circles/', list_circles, name=('list_circles')),
+	path('circles/create/', create_circle, name=('create_circle')),
 ]
--- cride/circles/views.py
+++ cride/circles/views.py
@@ -1,8 +1,10 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from cride.circles.models import Circle
 
 
+@api_view(['GET'])
 def list_circles(request):
 	"""List public circles."""
 	public_circles = Circle.objects.filter(is_public=True)
@@ -13,4 +15,21 @@ def list_circles(request):
 		'rides_taken': circle.rides_taken,
 		'members_limit': circle.members_limit
 	} for circle in public_circles]
-	return JsonResponse(data, safe=False)
+	return Response(data)
+
+
+@api_view(['POST'])
+def create_circle(request):
+	"""Create circle."""
+	name = request.data['name']
+	slug_name = request.data['slug_name']
+	about = request.data.get('about', '')
+	circle = Circle.objects.create(name=name, slug_name=slug_name, about=about)
+	data = {
+		'name': circle.name,
+		'slug_name': circle.slug_name,
+		'rides_offered': circle.rides_offered,
+		'rides_taken': circle.rides_taken,
+		'members_limit': circle.members_limit
+	}
+	return Response(data)
